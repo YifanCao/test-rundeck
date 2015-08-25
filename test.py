fc30564b6e2d4707acaa1eab955c783e584bbd71
zzyicao <yicao@microstrategy.com>
2015-08-25 16:43:33 -0400

    add new script

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python -d
+##
+# This script fetches a file on the local machine and send it to a remote server
+# --------------------------------------------------------------
+# Author: Yifan Cao
+# --------------------------------------------------------------
+
+def main():
+    print("test")
+
+
+if __name__ == '__main__':
+    main()
